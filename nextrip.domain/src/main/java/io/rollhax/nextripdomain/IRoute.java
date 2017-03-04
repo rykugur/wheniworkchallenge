7449a41c619b624987fce8815d67c783433cbb6f
rykugur <rollhax@gmail.com>
2017-03-04 11:43:28 -0600

    Finish IRoute/Route domain model impl

--- nextrip.domain/src/main/java/io/rollhax/nextripdomain/IRoute.java
+++ nextrip.domain/src/main/java/io/rollhax/nextripdomain/IRoute.java
@@ -1,4 +1,24 @@
 package io.rollhax.nextripdomain;
 
+import android.support.annotation.Nullable;
+
 public interface IRoute {
+
+    interface Json {
+        String DESCRIPTION = "Description";
+        String PROVIDER_ID = "ProviderID";
+        String ROUTE = "Route";
+    }
+
+    @Nullable
+    String getDescription();
+
+    /**
+     * The NexTrip API returns this as a string, but it's labeled an ID. That is odd.
+     */
+    @Nullable
+    String getProviderId();
+
+    @Nullable
+    String getRoute();
 }
--- nextrip.domain/src/main/java/io/rollhax/nextripdomain/Route.java
+++ nextrip.domain/src/main/java/io/rollhax/nextripdomain/Route.java
@@ -1,4 +1,35 @@
 package io.rollhax.nextripdomain;
 
+import android.support.annotation.Nullable;
+
+import com.google.gson.annotations.SerializedName;
+
 public class Route implements IRoute {
+
+    @SerializedName(Json.DESCRIPTION)
+    private String mDescription;
+    @SerializedName(Json.PROVIDER_ID)
+    private String mProviderId;
+    @SerializedName(Json.ROUTE)
+    private String mRoute;
+
+    //region IRoute
+    @Override
+    @Nullable
+    public String getDescription() {
+        return mDescription;
+    }
+
+    @Override
+    @Nullable
+    public String getProviderId() {
+        return mProviderId;
+    }
+
+    @Override
+    @Nullable
+    public String getRoute() {
+        return mRoute;
+    }
+    //endregion
 }

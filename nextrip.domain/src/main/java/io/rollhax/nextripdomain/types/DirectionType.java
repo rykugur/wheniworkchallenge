7d15a8ebe6c3d0c25fa7a82b3fb5dfad25b9c525
rykugur <rollhax@gmail.com>
2017-03-04 11:52:48 -0600

    Add DirectionType enum

---
+++ nextrip.domain/src/main/java/io/rollhax/nextripdomain/types/DirectionType.java
@@ -0,0 +1,32 @@
+package io.rollhax.nextripdomain.types;
+
+public enum DirectionType {
+    SOUTH(1),
+    EAST(2),
+    WEST(3),
+    NORTH(4),
+    UNKNOWN(-1);
+
+    private int mServerId;
+
+    DirectionType(int serverId) {
+        mServerId = serverId;
+    }
+
+    public int getmServerId() {
+        return mServerId;
+    }
+
+    public static DirectionType from(int serverId) {
+        DirectionType type = UNKNOWN;
+
+        for (DirectionType direction : values()) {
+            if (direction.mServerId == serverId) {
+                type = direction;
+                break;
+            }
+        }
+
+        return type;
+    }
+}
